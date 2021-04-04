1b30d62a38b6204aa537c574bf912359bbad9a69
oneflower <50647944+yihuahuahua@users.noreply.github.com>
2021-04-04 17:39:34 +0800

    Create main.py

---
+++ Log-analysis-master/main.py
@@ -0,0 +1,12 @@
+import time
+
+'''
+将一些 txt格式日志转换为 csv格式，并剔除不重要的信息
+'''
+def log_txt_to_csv(time1, time2, infile_way, outfile_way):
+    # 根据目录和时间，转换为对应的文件路径组
+    load_path = timechange(time1, time2, infile_way)
+    output_path = timechange(time1, time2, outfile_way, ".csv")
+    # 一对一转换
+    for infile, outfile in zip(load_path, output_path):
+        onefile_txt_to_csv(infile, outfile)
