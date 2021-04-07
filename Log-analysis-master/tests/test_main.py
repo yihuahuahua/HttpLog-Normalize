5c19d8dd1795f456ac7c89958d7f9e0d81c6b840
oneflower <50647944+yihuahuahua@users.noreply.github.com>
2021-04-07 16:18:49 +0800

    Add files via upload

--- Log-analysis-master/tests/test_main.py
+++ Log-analysis-master/tests/test_main.py
@@ -1,5 +1,7 @@
 from ..core.coreApi import *
 
 if __name__ == '__main__':
-    #将一些 txt 格式的日志转换为 csv 根式的日志， 并剔除一些不重要的信息
+    #将一些 txt 格式的日志转换为 csv 格式的日志， 并剔除一些不重要的信息
     log_txt_to_csv("2018-04-23", "2018-04-25", "logs/", "csv_logs/")
+    #将一些 csv 格式的日志转为一个日志，并根据 ip 排列
+    ip_classification("2018-04-23", "2018-04-23", "csv_logs/", "csv_ip_logs/")
